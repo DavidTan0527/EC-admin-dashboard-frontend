906377ba47591b6e3f7bc708e707d3ad3498ca25
DavidTan0527 <david_weiucheng@hotmail.com>
2024-06-11 15:19:25 +0800

    Regenerate project with vite-plugin-ssr

--- src/entry-client.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.scss'
-
-import { createApp } from './main'
-
-const { app } = createApp()
-
-// mount for client hydration
-app.mount('#app')
-
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import ssr from 'vite-plugin-ssr/plugin'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default {
+  plugins: [vue(), ssr()]
+}
